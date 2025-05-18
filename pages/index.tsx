db8d3ea3cb593a53c35ee22cfa2148d6aef0d850
Shlomia09 <shlomia09@gmail.com>
2025-05-18 20:01:58 +0200

    Delete pages directory

--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-gray-100">
-      <h1 className="text-2xl font-bold text-center">Welcome to NextGenAds Dashboard</h1>
-    </main>
-  );
-}
